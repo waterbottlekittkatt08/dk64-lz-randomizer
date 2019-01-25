73f54941630b6dbbcd5487d07d3ef0c3f484cf21
Alex Thieke <athieke@stevens.edu>
2019-01-25 01:32:09 -0500

    Changes to spoiler log: -Writes to text file instead of lua console -Includes exit names for maps with multiple distinct exits

---
+++ modules/mapAndExitNames.lua
@@ -0,0 +1,568 @@
+maps = {
+	"Test Map", -- 0
+	"Funky's Store",
+	"DK Arcade",
+	"K. Rool Barrel - Lanky's Maze",
+	"Jungle Japes - Mountain",
+	"Cranky's Lab",
+	"Jungle Japes - Minecart",
+	"Jungle Japes",
+	"Jungle Japes - Army Dillo",
+	"Jetpac",
+	"Kremling Kosh! (very easy)", -- 10
+	"Stealthy Snoop! (normal, no logo)",
+	"Jungle Japes - Shell",
+	"Jungle Japes - Lanky's Cave",
+	"Angry Aztec - Beetle Race",
+	"Snide's H.Q.",
+	"Angry Aztec - Tiny's Temple",
+	"Hideout Helm",
+	"Teetering Turtle Trouble! (very easy)",
+	"Angry Aztec - Five Door Temple (DK)",
+	"Angry Aztec - Llama Temple", -- 20
+	"Angry Aztec - Five Door Temple (Diddy)",
+	"Angry Aztec - Five Door Temple (Tiny)",
+	"Angry Aztec - Five Door Temple (Lanky)",
+	"Angry Aztec - Five Door Temple (Chunky)",
+	"Candy's Music Shop",
+	"Frantic Factory",
+	"Frantic Factory - Car Race",
+	"Hideout Helm (Level Intros, Game Over)",
+	"Frantic Factory - Power Shed",
+	"Gloomy Galleon", -- 30
+	"Gloomy Galleon - K. Rool's Ship",
+	"Batty Barrel Bandit! (easy)",
+	"Jungle Japes - Chunky's Cave",
+	"DK Isles Overworld",
+	"K. Rool Barrel - DK's Target Game",
+	"Frantic Factory - Crusher Room",
+	"Jungle Japes - Barrel Blast",
+	"Angry Aztec",
+	"Gloomy Galleon - Seal Race",
+	"Nintendo Logo", -- 40
+	"Angry Aztec - Barrel Blast",
+	"Troff 'n' Scoff", -- 42
+	"Gloomy Galleon - Shipwreck (Diddy, Lanky, Chunky)",
+	"Gloomy Galleon - Treasure Chest",
+	"Gloomy Galleon - Mermaid",
+	"Gloomy Galleon - Shipwreck (DK, Tiny)",
+	"Gloomy Galleon - Shipwreck (Lanky, Tiny)",
+	"Fungi Forest",
+	"Gloomy Galleon - Lighthouse",
+	"K. Rool Barrel - Tiny's Mushroom Game", -- 50
+	"Gloomy Galleon - Mechanical Fish",
+	"Fungi Forest - Ant Hill",
+	"Battle Arena - Beaver Brawl!",
+	"Gloomy Galleon - Barrel Blast",
+	"Fungi Forest - Minecart",
+	"Fungi Forest - Diddy's Barn",
+	"Fungi Forest - Diddy's Attic",
+	"Fungi Forest - Lanky's Attic",
+	"Fungi Forest - DK's Barn",
+	"Fungi Forest - Spider", -- 60
+	"Fungi Forest - Front Part of Mill",
+	"Fungi Forest - Rear Part of Mill",
+	"Fungi Forest - Mushroom Puzzle",
+	"Fungi Forest - Giant Mushroom",
+	"Stealthy Snoop! (normal)",
+	"Mad Maze Maul! (hard)",
+	"Stash Snatch! (normal)",
+	"Mad Maze Maul! (easy)",
+	"Mad Maze Maul! (normal)", -- 69
+	"Fungi Forest - Mushroom Leap", -- 70
+	"Fungi Forest - Shooting Game",
+	"Crystal Caves",
+	"Battle Arena - Kritter Karnage!",
+	"Stash Snatch! (easy)",
+	"Stash Snatch! (hard)",
+	"DK Rap",
+	"Minecart Mayhem! (easy)", -- 77
+	"Busy Barrel Barrage! (easy)",
+	"Busy Barrel Barrage! (normal)",
+	"Main Menu", -- 80
+	"Title Screen (Not For Resale Version)",
+	"Crystal Caves - Beetle Race",
+	"Fungi Forest - Dogadon",
+	"Crystal Caves - Igloo (Tiny)",
+	"Crystal Caves - Igloo (Lanky)",
+	"Crystal Caves - Igloo (DK)",
+	"Creepy Castle",
+	"Creepy Castle - Ballroom",
+	"Crystal Caves - Rotating Room",
+	"Crystal Caves - Shack (Chunky)", -- 90
+	"Crystal Caves - Shack (DK)",
+	"Crystal Caves - Shack (Diddy, middle part)",
+	"Crystal Caves - Shack (Tiny)",
+	"Crystal Caves - Lanky's Hut",
+	"Crystal Caves - Igloo (Chunky)",
+	"Splish-Splash Salvage! (normal)",
+	"K. Lumsy",
+	"Crystal Caves - Ice Castle",
+	"Speedy Swing Sortie! (easy)",
+	"Crystal Caves - Igloo (Diddy)", -- 100
+	"Krazy Kong Klamour! (easy)",
+	"Big Bug Bash! (very easy)",
+	"Searchlight Seek! (very easy)",
+	"Beaver Bother! (easy)",
+	"Creepy Castle - Tower",
+	"Creepy Castle - Minecart",
+	"Kong Battle - Battle Arena",
+	"Creepy Castle - Crypt (Lanky, Tiny)",
+	"Kong Battle - Arena 1",
+	"Frantic Factory - Barrel Blast", -- 110
+	"Gloomy Galleon - Pufftoss",
+	"Creepy Castle - Crypt (DK, Diddy, Chunky)",
+	"Creepy Castle - Museum",
+	"Creepy Castle - Library",
+	"Kremling Kosh! (easy)",
+	"Kremling Kosh! (normal)",
+	"Kremling Kosh! (hard)",
+	"Teetering Turtle Trouble! (easy)",
+	"Teetering Turtle Trouble! (normal)",
+	"Teetering Turtle Trouble! (hard)", -- 120
+	"Batty Barrel Bandit! (easy)",
+	"Batty Barrel Bandit! (normal)",
+	"Batty Barrel Bandit! (hard)",
+	"Mad Maze Maul! (insane)",
+	"Stash Snatch! (insane)",
+	"Stealthy Snoop! (very easy)",
+	"Stealthy Snoop! (easy)",
+	"Stealthy Snoop! (hard)",
+	"Minecart Mayhem! (normal)",
+	"Minecart Mayhem! (hard)", -- 130
+	"Busy Barrel Barrage! (hard)",
+	"Splish-Splash Salvage! (hard)",
+	"Splish-Splash Salvage! (easy)",
+	"Speedy Swing Sortie! (normal)",
+	"Speedy Swing Sortie! (hard)",
+	"Beaver Bother! (normal)",
+	"Beaver Bother! (hard)",
+	"Searchlight Seek! (easy)",
+	"Searchlight Seek! (normal)",
+	"Searchlight Seek! (hard)", -- 140
+	"Krazy Kong Klamour! (normal)",
+	"Krazy Kong Klamour! (hard)",
+	"Krazy Kong Klamour! (insane)",
+	"Peril Path Panic! (very easy)",
+	"Peril Path Panic! (easy)",
+	"Peril Path Panic! (normal)",
+	"Peril Path Panic! (hard)",
+	"Big Bug Bash! (easy)",
+	"Big Bug Bash! (normal)",
+	"Big Bug Bash! (hard)", -- 150
+	"Creepy Castle - Tunnel",
+	"Hideout Helm (Intro Story)",
+	"DK Isles (DK Theatre)",
+	"Frantic Factory - Mad Jack",
+	"Battle Arena - Arena Ambush!",
+	"Battle Arena - More Kritter Karnage!",
+	"Battle Arena - Forest Fracas!",
+	"Battle Arena - Bish Bash Brawl!",
+	"Battle Arena - Kamikaze Kremlings!",
+	"Battle Arena - Plinth Panic!", -- 160
+	"Battle Arena - Pinnacle Palaver!",
+	"Battle Arena - Shockwave Showdown!",
+	"Creepy Castle - Dungeon Basement",
+	"Creepy Castle - Tree",
+	"K. Rool Barrel - Diddy's Kremling Game",
+	"Creepy Castle - Chunky's Toolshed",
+	"Creepy Castle - Trash Can",
+	"Creepy Castle - Greenhouse",
+	"Jungle Japes Lobby",
+	"Hideout Helm Lobby", -- 170
+	"DK's House",
+	"Rock (Intro Story)",
+	"Angry Aztec Lobby",
+	"Gloomy Galleon Lobby",
+	"Frantic Factory Lobby",
+	"Training Grounds",
+	"Dive Barrel",
+	"Fungi Forest Lobby",
+	"Gloomy Galleon - Submarine",
+	"Orange Barrel", -- 180
+	"Barrel Barrel",
+	"Vine Barrel",
+	"Creepy Castle - Crypt",
+	"Enguarde Arena",
+	"Creepy Castle - Car Race",
+	"Crystal Caves - Barrel Blast",
+	"Creepy Castle - Barrel Blast",
+	"Fungi Forest - Barrel Blast",
+	"Fairy Island",
+	"Kong Battle - Arena 2", -- 190
+	"Rambi Arena",
+	"Kong Battle - Arena 3",
+	"Creepy Castle Lobby",
+	"Crystal Caves Lobby",
+	"DK Isles - Snide's Room",
+	"Crystal Caves - Army Dillo",
+	"Angry Aztec - Dogadon",
+	"Training Grounds (End Sequence)",
+	"Creepy Castle - King Kut Out",
+	"Crystal Caves - Shack (Diddy, upper part)", -- 200
+	"K. Rool Barrel - Diddy's Rocketbarrel Game",
+	"K. Rool Barrel - Lanky's Shooting Game",
+	"K. Rool Fight - DK Phase",
+	"K. Rool Fight - Diddy Phase",
+	"K. Rool Fight - Lanky Phase",
+	"K. Rool Fight - Tiny Phase",
+	"K. Rool Fight - Chunky Phase",
+	"Bloopers Ending",
+	"K. Rool Barrel - Chunky's Hidden Kremling Game",
+	"K. Rool Barrel - Tiny's Pony Tail Twirl Game", -- 210
+	"K. Rool Barrel - Chunky's Shooting Game",
+	"K. Rool Barrel - DK's Rambi Game",
+	"K. Lumsy Ending",
+	"K. Rool's Shoe",
+	"K. Rool's Arena", -- 215	
+};
+
+mapsWithMultipleExits = {
+	7, --Jungle Japes
+	26, --Frantic Factory
+	30, --Gloomy Galleon
+	34, --DK Isles Overworld
+	38, --Angry Aztec
+	43, --Gloomy Galleon - Shipwreck (Diddy, Lanky, Chunky)
+	46, --Gloomy Galleon - Shipwreck (DK, Tiny)
+	47, --Gloomy Galleon - Shipwreck (Lanky, Tiny)"
+	48, --Fungi Forest
+	61, --Fungi Forest - Front Part of Mill
+	62, --Fungi Forest - Rear Part of Mill
+	64, --Fungi Forest - Giant Mushroom
+	73, --Crystal Caves
+	87, --Creepy Castle
+	88, --Creepy Castle - Ballroom
+	112, --Creepy Castle - Crypt (DK, Diddy, Chunky)
+	113, --Creepy Castle - Museum
+	114, --Creepy Castle - Library
+	151, --Creepy Castle - Tunnel
+	169, --Jungle Japes Lobby
+	170, --Hideout Helm Lobby
+	173, --Angry Aztec Lobby
+	174, --Gloomy Galleon Lobby
+	175, --Frantic Factory Lobby
+	176, --Training Grounds
+	178, --Fungi Forest Lobby
+	183, --Creepy Castle - Crypt
+	193, --Creepy Castle Lobby
+	194, --Crystal Caves Lobby
+};
+
+exitTable = {}
+exitTable[7] = {
+	"From Japes Lobby (Intro)",
+	"From Shellhive",
+	"From Mountain",
+	"From Cranky",
+	"From Funky",
+	"From Painting Room",
+	"From Snide's",
+	"From BBlast",
+	"From Underground",
+	"From T&S (Diddy Cave)",
+	"From T&S (Near Cannon)",
+	"From ? (Other hill near SSSortie)",
+	"From T&S (Near Pool Fairy)",
+	"From ? (Near Pool Fairy)",
+	"From Minecart",
+	"From Japes Lobby",
+	"From DK Rap (DKTV Demo)",
+	"From Japes Lobby (Intro)"
+}
+exitTable[26] = {
+	"From Factory Lobby/Arcade",
+	"From Arcade Area (near Tiny BP)",
+	"From Tiny BP Area (To Arcade Area)",
+	"From Power Shed",
+	"From R&D Area (To Storage Room)",
+	"From Snide's",
+	"From Funky's",
+	"From Cranky's",
+	"From Crusher Room",
+	"From T&S (Block Tower)",
+	"From T&S (Arcade)",
+	"From T&S (R&D)",
+	"From T&S (Production Room)",
+	"From T&S (Storage Room)",
+	"From ? (Near Bad Hit Detection Man)",
+	"From BBlast",
+	"From Car Race",
+	"From Candy's",
+	"From Factory Lobby"
+}
+exitTable[30] = {
+	"From Galleon Lobby",
+	"From Diddy 5DS",
+	"From Chunky 5DS",
+	"From Lanky 5DS",
+	"From Pearls Chest",
+	"From Mermaid",
+	"From Tiny 5DS",
+	"From DK 5DS",
+	"From Tiny 2DS",
+	"From Lanky 2DS",
+	"From Lighthouse",
+	"From Seasick Ship",
+	"From T&S (Cactus)",
+	"From T&S (Near Cranky's)",
+	"From T&S (2DS)",
+	"From T&S (Enguarde Door)",
+	"From BBlast",
+	"From Snide's",
+	"From Candy's",
+	"From Seal Race",
+	"From T&S (Meme Hole)",
+	"From Submarine",
+	"From Cranky's",
+	"From Funky's",
+	"From Galleon Lobby"
+}
+exitTable[34] = {
+	"From Training Grounds",
+	"From K-Lumsy",
+	"From Japes Lobby",
+	"From Aztec Lobby",
+	"From Factory Lobby",
+	"From Galleon Lobby",
+	"From Fungi Lobby",
+	"From Helm Lobby",
+	"From BFI",
+	"From Snide's Room",
+	"From Caves Lobby",
+	"From Castle Lobby",
+	"From K Rool",
+	"From Training Grounds"
+}
+exitTable[38] = {
+	"From Aztec Lobby",
+	"From Tiny Temple",
+	"From Llama Temple",
+	"From Tiny 5DT",
+	"From Chunky 5DT",
+	"From DK 5DT",
+	"From Diddy 5DT",
+	"From Lanky 5DT",
+	"From Candy's",
+	"From Snide's",
+	"From Cranky's",
+	"From BBlast",
+	"From T&S (Candy's)",
+	"From T&S (W5)",
+	"From T&S (5DT)",
+	"From T&S (Cranky's)",
+	"From T&S (Funky's)",
+	"From Beetle Race",
+	"From Funky's",
+	"From Aztec Lobby"
+}
+exitTable[43] = {
+	"From Galleon (Diddy Entrance)",
+	"From Galleon (Chunky Entrance)",
+	"From Galleon (Lanky Entrance)",
+	"From Galleon (Diddy Entrance)"
+}
+exitTable[46] = {
+	"From Galleon (DK Entrance)",
+	"From Galleon (Tiny Entrance)",
+	"From Galleon (DK Entrance)"
+}
+exitTable[47] = {
+	"From Galleon (Tiny Entrance)",
+	"From Galleon (Lanky Entrance)",
+	"From Galleon (Tiny Entrance)"
+}
+exitTable[48] = {
+	"From Fungi Lobby",
+	"From Lanky Attic",
+	"From Diddy Attic",
+	"From Diddy Barn",
+	"From DK Barn",
+	"From Mill (Rear, PPunch Door)",
+	"From Mill (Front)",
+	"From Mill (Rear, Tiny Hole)",
+	"From G. Mush (Lower)",
+	"From G. Mush (Low Middle)",
+	"From G. Mush (Middle)",
+	"From G. Mush (High Middle)",
+	"From G. Mush (High)",
+	"From Face Puzzle",
+	"From Light Room (Lanky)",
+	"From Dark Room (Lanky)",
+	"From Minecart",
+	"From Cranky's",
+	"From Funky's",
+	"From Snide's",
+	"From T&S (DK Barn)",
+	"From T&S (Snide's)",
+	"From T&S (Beanstalk)",
+	"From Anthill",
+	"From T&S (G. Mush)",
+	"From T&S (Tree)",
+	"From BBlast",
+	"From Fungi Lobby (?)",
+	"From Fungi Lobby"
+}
+exitTable[61] = {
+	"From Fungi (W1 Door)",
+	"From Mill (Rear)",
+	"From Fungi (W1 Door)"
+}
+exitTable[62] = {
+	"From Fungi (PPunch Door)",
+	"From Spider Boss",
+	"From Mill (Front)",
+	"From Fungi (Tiny Door)",
+	"From Fungi (PPunch Door)"
+}
+exitTable[64] = {
+	"From Fungi (Low)",
+	"From Fungi (Middle)",
+	"From Fungi (Low Middle)",
+	"From Fungi (High Middle)",
+	"From Fungi (High)",
+	"From Fungi (Low)"
+}
+exitTable[73] = {
+	"From Caves Lobby",
+	"From Diddy 5DI",
+	"From DK 5DI",
+	"From Lanky 5DI",
+	"From Chunky 5DI",
+	"From Tiny 5DI",
+	"From Beetle Race",
+	"From ? (Near Rotating Room)",
+	"From ? (Near 1DC)",
+	"From ? (Near 5DC)",
+	"From ? (Near W3 Room)",
+	"From ? (5DI W3, Beta T&S)",
+	"From Cranky's",
+	"From Funky's",
+	"From DK 5DC",
+	"From Chunky 5DC",
+	"From Tiny 5DC",
+	"From Diddy 5DC (Lower)",
+	"From Diddy 5DC (Upper)",
+	"From Rotating Room",
+	"From 1DC",
+	"From Candy's",
+	"From Snide's",
+	"From T&S (Snide's)",
+	"From T&S (Rotating Room)",
+	"From T&S (1DC)",
+	"From T&S (Giant Boulder)",
+	"From ? (Behind W3 Room)",
+	"From BBlast",
+	"From ? (Giant Kosha Room)",
+	"From Ice Castle",
+	"From DK Treehouse (Secret Exit)",
+	"From Caves Lobby"
+}
+exitTable[87] = {
+	"From Castle Lobby",
+	"From Tree (Exit)",
+	"From Dungeon Tunnel (Close)",
+	"From T&S (W2)",
+	"From Crypt",
+	"From Dungeon Tunnel (Far)",
+	"From T&S (Rear)",
+	"From Museum",
+	"From Greenhouse (Front)",
+	"From Shed",
+	"From T&S (W4)",
+	"From Ballroom",
+	"From Library (Entrance)",
+	"From Library (Exit)",
+	"From Tower",
+	"From Tree (Entrance)",
+	"From Trash Can",
+	"From BBlast",
+	"From Cranky's",
+	"From Snide's",
+	"From Greenhouse (Rear)",
+	"From Castle Lobby (Intro)",
+	"From Castle Lobby"
+}
+exitTable[88] = {
+	"From Castle",
+	"From Museum (Monkeyport)",
+	"From Castle"
+}
+exitTable[112] = {
+	"From Crypt",
+	"From Minecart",
+	"From Crypt"
+}
+exitTable[113] = {
+	"From Castle",
+	"From Car Race",
+	"From Ballroom (Monkeyport)",
+	"From Castle"
+}
+exitTable[114] = {
+	"From Castle (Entrance)",
+	"From Castle (Exit)",
+	"From Castle (Entrance)"
+}
+exitTable[151] = {
+	"From Castle (Near)",
+	"From Candy's",
+	"From Castle (Far)",
+	"From T&S",
+	"From Dungeon",
+	"From Castle (Near)"
+}
+exitTable[169] = {
+	"From DK Isles",
+	"From Japes",
+	"From DK Isles"
+}
+exitTable[170] = {
+	"From DK Isles",
+	"From Helm",
+	"From DK Isles"
+}
+exitTable[173] = {
+	"From DK Isles",
+	"From Aztec",
+	"From DK Isles"
+}
+exitTable[174] = {
+	"From DK Isles",
+	"From Galleon",
+	"From DK Isles"
+}
+exitTable[175] = {
+	"From DK Isles",
+	"From Factory",
+	"From DK Isles"
+}
+exitTable[176] = {
+	"From DK Isles",
+	"From DK's Treehouse",
+	"From Cranky's",
+	"From DK Isles"
+}
+exitTable[178] = {
+	"From DK Isles",
+	"From Fungi",
+	"From DK Isles"
+}
+exitTable[183] = {
+	"From Castle",
+	"From Funky's",
+	"From T&S",
+	"From Crypt (DK/Diddy/Chunky)",
+	"From Crypt (Lanky/Tiny)",
+	"From Castle"
+}
+exitTable[193] = {
+	"From DK Isles",
+	"From Castle",
+	"From DK Isles"
+}
+exitTable[194] = {
+	"From DK Isles",
+	"From Caves",
+	"From DK Isles"
+}
--- modules/randomiser.lua
+++ modules/randomiser.lua
@@ -1,221 +1,4 @@
-maps = {
-		"Test Map", -- 0
-		"Funky's Store",
-		"DK Arcade",
-		"K. Rool Barrel - Lanky's Maze",
-		"Jungle Japes - Mountain",
-		"Cranky's Lab",
-		"Jungle Japes - Minecart",
-		"Jungle Japes",
-		"Jungle Japes - Army Dillo",
-		"Jetpac",
-		"Kremling Kosh! (very easy)", -- 10
-		"Stealthy Snoop! (normal, no logo)",
-		"Jungle Japes - Shell",
-		"Jungle Japes - Lanky's Cave",
-		"Angry Aztec - Beetle Race",
-		"Snide's H.Q.",
-		"Angry Aztec - Tiny's Temple",
-		"Hideout Helm",
-		"Teetering Turtle Trouble! (very easy)",
-		"Angry Aztec - Five Door Temple (DK)",
-		"Angry Aztec - Llama Temple", -- 20
-		"Angry Aztec - Five Door Temple (Diddy)",
-		"Angry Aztec - Five Door Temple (Tiny)",
-		"Angry Aztec - Five Door Temple (Lanky)",
-		"Angry Aztec - Five Door Temple (Chunky)",
-		"Candy's Music Shop",
-		"Frantic Factory",
-		"Frantic Factory - Car Race",
-		"Hideout Helm (Level Intros, Game Over)",
-		"Frantic Factory - Power Shed",
-		"Gloomy Galleon", -- 30
-		"Gloomy Galleon - K. Rool's Ship",
-		"Batty Barrel Bandit! (easy)",
-		"Jungle Japes - Chunky's Cave",
-		"DK Isles Overworld",
-		"K. Rool Barrel - DK's Target Game",
-		"Frantic Factory - Crusher Room",
-		"Jungle Japes - Barrel Blast",
-		"Angry Aztec",
-		"Gloomy Galleon - Seal Race",
-		"Nintendo Logo", -- 40
-		"Angry Aztec - Barrel Blast",
-		"Troff 'n' Scoff", -- 42
-		"Gloomy Galleon - Shipwreck (Diddy, Lanky, Chunky)",
-		"Gloomy Galleon - Treasure Chest",
-		"Gloomy Galleon - Mermaid",
-		"Gloomy Galleon - Shipwreck (DK, Tiny)",
-		"Gloomy Galleon - Shipwreck (Lanky, Tiny)",
-		"Fungi Forest",
-		"Gloomy Galleon - Lighthouse",
-		"K. Rool Barrel - Tiny's Mushroom Game", -- 50
-		"Gloomy Galleon - Mechanical Fish",
-		"Fungi Forest - Ant Hill",
-		"Battle Arena - Beaver Brawl!",
-		"Gloomy Galleon - Barrel Blast",
-		"Fungi Forest - Minecart",
-		"Fungi Forest - Diddy's Barn",
-		"Fungi Forest - Diddy's Attic",
-		"Fungi Forest - Lanky's Attic",
-		"Fungi Forest - DK's Barn",
-		"Fungi Forest - Spider", -- 60
-		"Fungi Forest - Front Part of Mill",
-		"Fungi Forest - Rear Part of Mill",
-		"Fungi Forest - Mushroom Puzzle",
-		"Fungi Forest - Giant Mushroom",
-		"Stealthy Snoop! (normal)",
-		"Mad Maze Maul! (hard)",
-		"Stash Snatch! (normal)",
-		"Mad Maze Maul! (easy)",
-		"Mad Maze Maul! (normal)", -- 69
-		"Fungi Forest - Mushroom Leap", -- 70
-		"Fungi Forest - Shooting Game",
-		"Crystal Caves",
-		"Battle Arena - Kritter Karnage!",
-		"Stash Snatch! (easy)",
-		"Stash Snatch! (hard)",
-		"DK Rap",
-		"Minecart Mayhem! (easy)", -- 77
-		"Busy Barrel Barrage! (easy)",
-		"Busy Barrel Barrage! (normal)",
-		"Main Menu", -- 80
-		"Title Screen (Not For Resale Version)",
-		"Crystal Caves - Beetle Race",
-		"Fungi Forest - Dogadon",
-		"Crystal Caves - Igloo (Tiny)",
-		"Crystal Caves - Igloo (Lanky)",
-		"Crystal Caves - Igloo (DK)",
-		"Creepy Castle",
-		"Creepy Castle - Ballroom",
-		"Crystal Caves - Rotating Room",
-		"Crystal Caves - Shack (Chunky)", -- 90
-		"Crystal Caves - Shack (DK)",
-		"Crystal Caves - Shack (Diddy, middle part)",
-		"Crystal Caves - Shack (Tiny)",
-		"Crystal Caves - Lanky's Hut",
-		"Crystal Caves - Igloo (Chunky)",
-		"Splish-Splash Salvage! (normal)",
-		"K. Lumsy",
-		"Crystal Caves - Ice Castle",
-		"Speedy Swing Sortie! (easy)",
-		"Crystal Caves - Igloo (Diddy)", -- 100
-		"Krazy Kong Klamour! (easy)",
-		"Big Bug Bash! (very easy)",
-		"Searchlight Seek! (very easy)",
-		"Beaver Bother! (easy)",
-		"Creepy Castle - Tower",
-		"Creepy Castle - Minecart",
-		"Kong Battle - Battle Arena",
-		"Creepy Castle - Crypt (Lanky, Tiny)",
-		"Kong Battle - Arena 1",
-		"Frantic Factory - Barrel Blast", -- 110
-		"Gloomy Galleon - Pufftoss",
-		"Creepy Castle - Crypt (DK, Diddy, Chunky)",
-		"Creepy Castle - Museum",
-		"Creepy Castle - Library",
-		"Kremling Kosh! (easy)",
-		"Kremling Kosh! (normal)",
-		"Kremling Kosh! (hard)",
-		"Teetering Turtle Trouble! (easy)",
-		"Teetering Turtle Trouble! (normal)",
-		"Teetering Turtle Trouble! (hard)", -- 120
-		"Batty Barrel Bandit! (easy)",
-		"Batty Barrel Bandit! (normal)",
-		"Batty Barrel Bandit! (hard)",
-		"Mad Maze Maul! (insane)",
-		"Stash Snatch! (insane)",
-		"Stealthy Snoop! (very easy)",
-		"Stealthy Snoop! (easy)",
-		"Stealthy Snoop! (hard)",
-		"Minecart Mayhem! (normal)",
-		"Minecart Mayhem! (hard)", -- 130
-		"Busy Barrel Barrage! (hard)",
-		"Splish-Splash Salvage! (hard)",
-		"Splish-Splash Salvage! (easy)",
-		"Speedy Swing Sortie! (normal)",
-		"Speedy Swing Sortie! (hard)",
-		"Beaver Bother! (normal)",
-		"Beaver Bother! (hard)",
-		"Searchlight Seek! (easy)",
-		"Searchlight Seek! (normal)",
-		"Searchlight Seek! (hard)", -- 140
-		"Krazy Kong Klamour! (normal)",
-		"Krazy Kong Klamour! (hard)",
-		"Krazy Kong Klamour! (insane)",
-		"Peril Path Panic! (very easy)",
-		"Peril Path Panic! (easy)",
-		"Peril Path Panic! (normal)",
-		"Peril Path Panic! (hard)",
-		"Big Bug Bash! (easy)",
-		"Big Bug Bash! (normal)",
-		"Big Bug Bash! (hard)", -- 150
-		"Creepy Castle - Dungeon",
-		"Hideout Helm (Intro Story)",
-		"DK Isles (DK Theatre)",
-		"Frantic Factory - Mad Jack",
-		"Battle Arena - Arena Ambush!",
-		"Battle Arena - More Kritter Karnage!",
-		"Battle Arena - Forest Fracas!",
-		"Battle Arena - Bish Bash Brawl!",
-		"Battle Arena - Kamikaze Kremlings!",
-		"Battle Arena - Plinth Panic!", -- 160
-		"Battle Arena - Pinnacle Palaver!",
-		"Battle Arena - Shockwave Showdown!",
-		"Creepy Castle - Basement",
-		"Creepy Castle - Tree",
-		"K. Rool Barrel - Diddy's Kremling Game",
-		"Creepy Castle - Chunky's Toolshed",
-		"Creepy Castle - Trash Can",
-		"Creepy Castle - Greenhouse",
-		"Jungle Japes Lobby",
-		"Hideout Helm Lobby", -- 170
-		"DK's House",
-		"Rock (Intro Story)",
-		"Angry Aztec Lobby",
-		"Gloomy Galleon Lobby",
-		"Frantic Factory Lobby",
-		"Training Grounds",
-		"Dive Barrel",
-		"Fungi Forest Lobby",
-		"Gloomy Galleon - Submarine",
-		"Orange Barrel", -- 180
-		"Barrel Barrel",
-		"Vine Barrel",
-		"Creepy Castle - Crypt",
-		"Enguarde Arena",
-		"Creepy Castle - Car Race",
-		"Crystal Caves - Barrel Blast",
-		"Creepy Castle - Barrel Blast",
-		"Fungi Forest - Barrel Blast",
-		"Fairy Island",
-		"Kong Battle - Arena 2", -- 190
-		"Rambi Arena",
-		"Kong Battle - Arena 3",
-		"Creepy Castle Lobby",
-		"Crystal Caves Lobby",
-		"DK Isles - Snide's Room",
-		"Crystal Caves - Army Dillo",
-		"Angry Aztec - Dogadon",
-		"Training Grounds (End Sequence)",
-		"Creepy Castle - King Kut Out",
-		"Crystal Caves - Shack (Diddy, upper part)", -- 200
-		"K. Rool Barrel - Diddy's Rocketbarrel Game",
-		"K. Rool Barrel - Lanky's Shooting Game",
-		"K. Rool Fight - DK Phase",
-		"K. Rool Fight - Diddy Phase",
-		"K. Rool Fight - Lanky Phase",
-		"K. Rool Fight - Tiny Phase",
-		"K. Rool Fight - Chunky Phase",
-		"Bloopers Ending",
-		"K. Rool Barrel - Chunky's Hidden Kremling Game",
-		"K. Rool Barrel - Tiny's Pony Tail Twirl Game", -- 210
-		"K. Rool Barrel - Chunky's Shooting Game",
-		"K. Rool Barrel - DK's Rambi Game",
-		"K. Lumsy Ending",
-		"K. Rool's Shoe",
-		"K. Rool's Arena", -- 215	
-};
+require "modules.mapAndExitNames";
 
 regular_map_table = {
 	[1] = 0x0600,
@@ -639,40 +422,54 @@ function setAssortments()
 	generateKRoolOrder();
 end
 
-function displaySeedSpoiler()
-	print("");
-	print("~~~~~~~~~~~~~");
-	print("SEED SPOILERS");
-	print("Seed Number: "..seedAsNumber);
-	print("");
-	print("REGULAR MAP ASSORTMENT");
+function getSeedSpoiler()
+	print("Writing spoiler to file...");
+	file = io.open("spoiler.txt", "w+")
+	file:write("~~~~~~~~~~~~~", "\n");
+	file:write("SEED SPOILERS", "\n");
+	file:write("Seed Number: "..seedAsNumber, "\n");
+	file:write("\n");
+	file:write("REGULAR MAP ASSORTMENT", "\n");
 	for i = 1, #regular_map_assortment do
 		lz_map_in = math.floor(regular_map_table[i] / 256);
 		lz_exit_in = regular_map_table[i] - (256 * lz_map_in);
 		lz_map_out = math.floor(regular_map_table[regular_map_assortment[i]] / 256);
 		lz_exit_out = regular_map_table[regular_map_assortment[i]] - (256 * lz_map_out);
-		print("");
-		print("LZ to: "..maps[lz_map_in + 1].." (Exit "..lz_exit_in..")");
-		print("Goes to: "..maps[lz_map_out + 1].." (Exit "..lz_exit_out..")");
+		file:write("\n");
+		file:write("LZ to: "..maps[lz_map_in + 1].." (Exit "..getExitName(lz_map_in, lz_exit_in)..")", "\n");
+		file:write("Goes to: "..maps[lz_map_out + 1].." (Exit "..getExitName(lz_map_out, lz_exit_out)..")", "\n");
 	end
-	print("");
-	print("BONUS MAP ASSORTMENT");
+	file:write("\n");
+	file:write("BONUS MAP ASSORTMENT", "\n");
 	for i = 1, #bonus_map_assortment do
 		lz_map_in = bonus_map_table[i];
 		lz_map_out = bonus_map_table[bonus_map_assortment[i]];
-		print("");
-		print("LZ to: "..maps[lz_map_in + 1]);
-		print("Goes to: "..maps[lz_map_out + 1]);
+		file:write("\n");
+		file:write("LZ to: "..maps[lz_map_in + 1], "\n");
+		file:write("Goes to: "..maps[lz_map_out + 1], "\n");
 	end
-	print("");
-	print("BOSS MAP ASSORTMENT");
+	file:write("\n");
+	file:write("BOSS MAP ASSORTMENT", "\n");
 	for i = 1, #boss_map_assortment do
 		lz_map_in = boss_map_table[i];
 		lz_map_out = boss_map_table[boss_map_assortment[i]];
-		print("");
-		print("LZ to: "..maps[lz_map_in + 1]);
-		print("Goes to: "..maps[lz_map_out + 1]);
+		file:write("\n");
+		file:write("LZ to: "..maps[lz_map_in + 1], "\n");
+		file:write("Goes to: "..maps[lz_map_out + 1], "\n");
 	end
+	file:close();
+	print("Saved spoiler log to spoiler.txt!");
+end
+
+function getExitName(map_index, exit_index)
+	for i = 1, #mapsWithMultipleExits  do
+        if map_index == mapsWithMultipleExits[i] then
+			local exitName = exitTable[map_index][exit_index + 1];
+			assert(exitName ~= nil, "Invalid or unknown exit! Map: "..maps[map_index + 1]..", Exit: " ..exit_index);
+			return exitName;
+		end
+    end
+    return exit_index;
 end
 
 function getLoadingZone(destmap, destexit)
