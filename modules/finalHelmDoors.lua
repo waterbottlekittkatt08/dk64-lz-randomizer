86ee3e9cbff1d4335c895a455d4148948c679f26
theballaam96 <theballaam96@gmail.com>
2019-02-22 20:51:27 +0000

    Bug Fix
    - Coin door now opens (There's an ingame failsafe to ensure you have both company coins in order for the door to be open. Currently locks out Arcade GB, but will work on a fix for that)

--- modules/finalHelmDoors.lua
+++ modules/finalHelmDoors.lua
@@ -1,6 +1,8 @@
 if settings.gameLengths < 3 then	
 	fileFlagsCount = #newFileFlags;
 	newFileFlags[fileFlagsCount + 1] = {0x60,3}; -- Coin Door
+	newFileFlags[fileFlagsCount + 2] = {0x10,4}; -- Nintendo Coin (Failsafe)
+	newFileFlags[fileFlagsCount + 3] = {0x2F,3}; -- Rareware Coin (Failsafe)
 end
 
 if settings.gameLengths < 2 then
